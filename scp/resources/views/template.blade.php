39227604e73cf8b613341a13d694f86ad343e6f2
Lenovo Y540 <lightersenior@outlook.com>
2021-05-28 19:46:08 +0700

    Begin The Project

---
+++ scp/resources/views/template.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>@yield('title')</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <style>
+    .bg-color {
+        background-color: #efefef;
+        height: 95vh;
+    }
+    </style>
+</head>
+<body>
+    @include('navbar')
+
+    <div class="bg-color">
+        <div class="container bg-white h-100 p-3">
+            @yield('page')
+        </div>
+    </div>
+</body>
+</html>
